5bd7df35436c00efcd399813ec211bbfd1437ecf
Seb <seb@dfd.co.nz>
2022-04-27 22:11:11 +1200

    Portfolio cards - progress commit - change HTML to template

--- js/portfolio-card.js
+++ js/portfolio-card.js
@@ -2,64 +2,73 @@ class PortfolioCard extends HTMLElement {
   constructor() {
     super();
     let shadowDom = this.attachShadow({ mode: "open" });
+    shadowDom.innerHTML = `
+    <template id="portfolio-card-template">
+        <link href="css/sanitize.css" rel="stylesheet" />
+        <link href="css/assets.css" rel="stylesheet" />
+        <link href="css/forms.css" rel="stylesheet" />
+        <link href="css/typography.css" rel="stylesheet" />
+
+        <style>
+            .card {
+                background-color: white;
+                padding: var(--page-padding-y);
+                border: solid 1px grey;
+                border-radius: 3px;
+            }
+
+            .card__context {
+                text-transform: uppercase;
+                color: grey;
+                font-size: smaller;
+            }
+
+            .card__title {
+                font-size: larger;
+                margin-bottom: var(--page-padding-y);
+            }
+
+            .card__description {
+            }
+
+            .card__actions a {
+                display: inline-block;
+                background-color: grey;
+                border-radius: 3px;
+                padding: var(--spacing-multiple-y) var(--spacing-multiple-x);
+                color: white;
+                text-decoration: none;
+            }
+        </style>
+    
+        <article class="card" id="article-container">
+        <div class="card__content">
+            <div class="card__context">
+                <slot name="context"></slot>
+            </div>
+            <div class="card__title">
+                <slot name="title"></slot>
+            </div>
+            <div class="card_description">
+                <slot name="description"></slot>
+            </div>
+            <div class="card__techs">
+                <slot name="tech-icons"></slot>
+            </div>
+            <div class="card__actions">
+                <slot name="actions"></slot>
+            </div>
+        </div>
+        </article>
+    </template>
+    `;
   }
 
   connectedCallback() {
-    this.shadowRoot.innerHTML = `  
-    <link href="css/sanitize.css" rel="stylesheet" />
-    <link href="css/assets.css" rel="stylesheet" />
-    <link href="css/forms.css" rel="stylesheet" />
-    <link href="css/typography.css" rel="stylesheet" />
-
-    <style>
-        .card {
-            background-color: white;
-            padding: var(--page-padding-y);
-            border: solid 1px grey;
-            border-radius: 3px;
-        }
-
-        .card__context {
-            text-transform: uppercase;
-            color: grey;
-            font-size: smaller;
-        }
-
-        .card__title {
-            font-size: larger;
-            margin-bottom: var(--page-padding-y);
-        }
-
-        .card__description {
-        }
-
-        .card__actions a {
-            display: inline-block;
-            background-color: grey;
-            border-radius: 3px;
-            padding: var(--spacing-multiple-y) var(--spacing-multiple-x);
-            color: white;
-            text-decoration: none;
-        }
-    </style>
-  
-    <article class="card" id="article-container">
-    <div class="card__content">
-        <div class="card__context">Side Project</div>
-        <div class="card__title">Web Component</div>
-        <div class="card_description">card 1 text text text  text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text</div>
-        <div class="card__techs">
-            <img src="/img/tech-logos/docker.svg" alt="Docker logo" style="width:1em;" />
-            <img src="/img/tech-logos/rabbitmq.svg" alt="RabbitMQ logo" style="width:1em;" />
-            <img src="/img/tech-logos/teamcity.svg" alt="TeamCity logo" style="width:1em;" />
-        </div>
-        <div class="card__actions">
-            <a href="#">View Site</a>
-            <a href="#">View Code</a>
-        </div>
-    </div>
-    </article>
-    `;
+    const template = this.shadowRoot.getElementById("portfolio-card-template");
+    //console.debug(`PortfolioCard.connectedCallback: template = `, template);
+    const node = document.importNode(template.content, true);
+    this.shadowRoot.appendChild(node);
   }
 
   resize() {
