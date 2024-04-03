4e7de5e1541a57c6ccc63c25ce5e82a63e498252
Seb <seb@dfd.co.nz>
2024-04-03 22:04:31 +1300

    Remove contact form component; add standard button css to portfolio-card component

--- js/portfolio-card.js
+++ js/portfolio-card.js
@@ -6,6 +6,7 @@ class PortfolioCard extends HTMLElement {
       <link href="css/sanitize.css" rel="stylesheet" />
       <link href="css/assets.css" rel="stylesheet" />
       <link href="css/forms.css" rel="stylesheet" />
+      <link href="css/standard-button.css" rel="stylesheet" />
 
       <style>
           .card {
